47536947f4ce46e2e10e6d2177860e476d31c755
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-05-05 15:31:01 -0400

    se arregla codigo agregado validación en handlebusqueda de que los campos no sean undefined para que realice la busqueda adecuadamente

--- render_dinamico/src/assets/componentes/buscador/Buscador.jsx
+++ render_dinamico/src/assets/componentes/buscador/Buscador.jsx
@@ -4,23 +4,28 @@ const Buscador = ({ baseColaboradores, setFiltroColaboradores }) => {
   const [busqueda, setBusqueda] = useState("");
 
   const handleBusqueda = (e) => {
-    const valor = e.target.value;
+    const valor = e.target.value.toLowerCase(); // Convertir la búsqueda a minúsculas
     setBusqueda(valor);
 
     // Filtrar los colaboradores según la búsqueda en cualquier campo
     const colaboradoresFiltrados = baseColaboradores.filter((colaborador) => {
+            // Validar cada campo para asegurarse de que no sea undefined antes de llamar a toLowerCase()
+
       return (
-        colaborador.id.toLowerCase().includes(valor.toLowerCase()) ||
-        colaborador.nombre.toLowerCase().includes(valor.toLowerCase()) ||
-        colaborador.email.toLowerCase().includes(valor.toLowerCase()) ||
-        colaborador.edad.toString().includes(valor.toLowerCase()) ||
-        colaborador.cargo.toLowerCase().includes(valor.toLowerCase()) ||
-        colaborador.telefono.toLowerCase().includes(valor.toLowerCase())
+        (colaborador.id && colaborador.id.toLowerCase().includes(valor)) ||
+        (colaborador.nombre && colaborador.nombre.toLowerCase().includes(valor)) ||
+        (colaborador.email && colaborador.email.toLowerCase().includes(valor)) ||
+        (colaborador.edad && colaborador.edad.toString().includes(valor)) ||
+        (colaborador.cargo && colaborador.cargo.toLowerCase().includes(valor)) ||
+        (colaborador.telefono && colaborador.telefono.toLowerCase().includes(valor))
       );
     });
 
     // Actualizar el estado de los colaboradores filtrados
     setFiltroColaboradores(colaboradoresFiltrados);
+    // Imprimir mensaje de depuración
+    console.log("Búsqueda realizada:", valor);
+    console.log("Colaboradores filtrados:", colaboradoresFiltrados);
   };
 
   return (
