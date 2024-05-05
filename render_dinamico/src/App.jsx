56f481a7dc51a3fb272cf5d0e2d2e55e83c5b4f4
Droopytex <a.velasquezvega@gmail.com>
2024-05-05 01:13:02 -0400

    preparando elementos para defafío (tabla)

--- render_dinamico/src/App.jsx
+++ render_dinamico/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-
 import "./App.css";
-
 import { BaseColaboradores } from "./BaseColaboradores";
 import Formulario from "./assets/componentes/formulario/Formulario";
+import Listado from "./assets/componentes/listado/Listado";
+
 function App() {
-  console.log(BaseColaboradores);
+  const [baseCol, setBaseCol] = useState(BaseColaboradores);
+  console.log(baseCol);
 
   return (
     <>
+      <Listado />
       <Formulario />
     </>
   );
--- render_dinamico/src/assets/componentes/listado/Listado.jsx
+++ render_dinamico/src/assets/componentes/listado/Listado.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 function Listado() {
-  return <></>;
+  return (
+    <>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Edad</th>
+            <th scope="col">Cargo</th>
+            <th scope="col">Teléfono</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Listado;
